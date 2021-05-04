06d541025e2787bd130609b3c9e306d7f914c474
Thomas Semiz <tas56@njit.edu>
2021-05-04 15:35:12 -0400

    Added passport configuration

--- src/index.js
+++ src/index.js
@@ -40,6 +40,26 @@ if (app.get("env") === "production") {
  * Passport Configuration (New!)
  */
 
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
 /**
  *  App Configuration
  */
@@ -47,6 +67,20 @@ if (app.get("env") === "production") {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 /**
  * Routes Definitions
