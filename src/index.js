0c86f6856a43b68d2298edd27f88efeb1402a0f8
Thomas Semiz <tas56@njit.edu>
2021-05-04 15:12:03 -0400

    Updated views and CSS'

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const port = process.env.PORT || "8000";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes Definitions
@@ -27,6 +28,10 @@ app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
 /**
