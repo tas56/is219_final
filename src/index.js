d66b8bdb01aba99d2c58eeaeac3cb0bb8702f224
Thomas Semiz <tas56@njit.edu>
2021-05-04 15:28:16 -0400

    Auth0 session configuration

--- src/index.js
+++ src/index.js
@@ -7,12 +7,38 @@
 const express = require("express");
 const path = require("path");
 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 /**
  * App Variables
  */
 
 const app = express();
 const port = process.env.PORT || "8000";
+
+/**
+ * Session Configuration (New!)
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration (New!)
+ */
 
 /**
  *  App Configuration
